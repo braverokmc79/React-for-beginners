fc041e561c784955a32b91dbfa5768890b4a6891
braverokmc79 <braverokmc79@gmail.com>
2022-08-27 23:04:09 +0900

    8.props

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,21 +1,24 @@
 import { useState } from "react";
+import UserName from "./UserName";
 
-const Hello = () => {
-    //let name = "Mike";
+const Hello = ({ age }) => {
+    //console.log(props);
     const [name, setName] = useState("Mike");
-
-    // function changeName() {
-    //     const newName = name === "Mike" ? "Jane" : "Mike";
-    //     //document.getElementById("name").innerText = newName;
-    //     setName(newName);
-    // }
+    //const [age, setAge] = useState(props.age);
+    const msg = age > 19 ? "성인 입니다." : "미성연자 입니다.";
 
     return (
         <div>
             <h1>state</h1>
-            <h2 id="name">{name}</h2>
+            <h2 id="name">
+                {name} ({age})  : {msg}
+            </h2>
+
+            <UserName name={name} />
+
             <button onClick={() => {
                 setName(name === "Mike" ? "Jane" : "Mike");
+                //setAge(age + 1);
             }}>Change</button>
         </div >
     );
---
+++ src/component/UserName.js
@@ -0,0 +1,9 @@
+
+
+const UserName = ({ name }) => {
+    return (
+        <p> Hello {name}</p>
+    );
+};
+
+export default UserName;
