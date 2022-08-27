5394978ae62861534f0fdd21883d3720c99239ce
braverokmc79 <braverokmc79@gmail.com>
2022-08-27 21:53:07 +0900

    6.이벤트 처리(Handling Events)

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,24 +1,33 @@
 
-import React from 'react';
-import styles from "./Hello.module.css";
+
 
 const Hello = () => {
+
+    function showName() {
+        console.log("Mike");
+    }
+
+    function showAge(age) {
+        console.log(age);
+    }
+
+    function showText(txt) {
+        console.log(txt);
+    }
+
     return (
         <div>
-            <h1
-                style={
-                    {
-                        color: '#f00',
-                        borderRight: '2px solid #000',
-                        marginBottom: "30px",
-                        opacity: 1
-                    }
-                }
-            >Hello</h1>
-
-            <div className={styles.box} >Hello</div>
-
-        </div>
+            <h1>Hello</h1>
+            <button onClick={showName}>Show name</button>
+            <button onClick={() => {
+                showAge(30);
+            }} >Show age</button>
+            <br /> <br />
+            <input type="text" onChange={(e) => {
+                const txt = e.target.value;
+                showText(txt);
+            }} />
+        </div >
     );
 
 };
