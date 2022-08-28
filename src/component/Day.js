aed2837933b677b644b2ffd0c3bf09219b50e105
braverokmc79 <braverokmc79@gmail.com>
2022-08-28 11:43:38 +0900

    12. useEffect, fetch()로 API 호출

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,23 +1,33 @@
-import dummy from '../db/data.json';
-import { useLocation, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import Word from './Word';
+import { useState, useEffect } from 'react';
+
 const Day = () => {
-    // const location = useLocation();
-    // const params = useParams();
-    // console.log("params ", params, "location ", location);
-
-
     const { day } = useParams();
-    const wordList = dummy.words.filter(word => {
-        return Number(word.day) === Number(day)
-    });
+    // const wordList = dummy.words.filter(word => {
+    //     return Number(word.day) === Number(day)
+    // });
+    const [words, setWords] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:3001/words?day=${day}`)
+            .then((res) => res.json())
+            .then(data => {
+                //console.log(data);
+                setWords(data);
+            }).catch(err => {
+                console.error("에러:", err);
+            });
+
+    }, [day]);
+
 
     return (
         <>
             <h2>Day {day}</h2>
             <table>
                 <tbody>
-                    {wordList.map(word => (
+                    {words.map(word => (
                         <Word key={word.id} word={word} />
                     ))}
                 </tbody>
--- src/component/DayList.js
+++ src/component/DayList.js
@@ -1,18 +1,52 @@
-import dummy from "../db/data.json";
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 const DayList = () => {
+    const [days, setDays] = useState([]);
+    /*
+     const [count, setCount] = useState(0);
+ 
+     function onClick() {
+         setCount(count + 1);
+     }
+ 
+     function onClick2() {
+         setDays([...days, {
+             id: new Date(),
+             day: (Math.floor(Math.random() * 31)) + 1,
+         }]);
+     }
+ 
+     useEffect(() => {
+         console.log("Counter Chnage");
+     }, [count])
+ 
+     */
+    useEffect(() => {
+        fetch("http://localhost:3001/days")
+            .then((res) => res.json())
+            .then(data => {
+                // console.log("data :", data);
+                setDays(data);
+            }).catch(err => {
+                console.error("에러 :", err);
+            });
+    }, []);
 
     return (
-        <ul className="list_day">
-            {dummy.days.map(day => (
-                <li key={day.id}>
-                    <Link to={`/day/${day.day}`}>Day {day.day}</Link>
-                </li>
-            ))
-            }
+        <>
+            <ul className="list_day">
+                {days.map(day => (
+                    <li key={day.id} id={day.id}>
+                        <Link to={`/day/${day.day}`}>Day {day.day}</Link>
+                    </li>
+                ))
+                }
 
-        </ul >
+            </ul >
+            {/* <button onClick={onClick}>{count}</button>
+            <button onClick={onClick2}>Day Change</button> */}
+        </>
     );
 };
 
