ad8156eada938f145a5f73492d1de70d4c723181
braverokmc79 <braverokmc79@gmail.com>
2022-08-28 12:48:58 +0900

    14. PUT(수정), DELETE(삭제)

--- src/component/Word.js
+++ src/component/Word.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useState } from 'react';
 
-const Word = ({ word }) => {
+const Word = ({ word: w }) => {
+    const [word, setWord] = useState(w);
     const [isShow, setIsShow] = useState(false);
     const [isDone, setIsDone] = useState(word.isDone);
 
@@ -11,7 +12,40 @@ const Word = ({ word }) => {
     }
 
     function toggleDone() {
-        setIsDone(!isDone);
+
+        fetch(`http://localhost:3001/words/${word.id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json; charset=utf-8"
+            },
+            body: JSON.stringify({
+                ...word,
+                isDone: !isDone
+            }),
+
+        }).then((res) => {
+            if (res.ok) {
+                setIsDone(!isDone);
+            }
+        })
+    }
+
+
+    function del() {
+        if (window.confirm("삭제 하시겠습니까?")) {
+            fetch(`http://localhost:3001/words/${word.id}`, {
+                method: "DELETE",
+            }).then((res) => {
+                if (res.ok) {
+                    setWord({ id: 0 });
+                }
+            });
+        }
+    }
+
+
+    if (word.id === 0) {
+        return null;
     }
 
     return (
@@ -25,7 +59,7 @@ const Word = ({ word }) => {
             <td>{isShow && word.kor}</td>
             <td>
                 <button onClick={toggleShow}>뜻 {isShow ? '숨기기' : '보기'}</button>
-                <button className='btn_del'>삭제</button>
+                <button onClick={del} className='btn_del'>삭제</button>
             </td>
         </tr>
     );
